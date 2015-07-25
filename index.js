f9f7939404f6d98162f7017372ca5686514775c1
Jason Smith (iMac) <jason.h.smith@gmail.com>
2015-07-26 00:17:12 +0700

    Parse the HTML and build an array of the package stack

--- index.js
+++ index.js
@@ -2,11 +2,67 @@ module.exports = check_balance
 
 var debug = require('debug')('hlife')
 var request = require('request')
+var cheerio = require('cheerio')
 
 var SITE = 'http://hlife.truecorp.co.th'
 var UA   = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0'
 
+var TH = { name     : /แพ็กเกจ/
+         , remaining: /คงเหลือ/
+         , validity : /ระยะเวลาใช้งาน/
+         }
+
 function check_balance(callback) {
+  fetch_balance(function(er, html) {
+    if (er)
+      return callback(er)
+
+    var $ = cheerio.load(html)
+    var result = []
+    var groups = $('.control-group')
+    debug('Found %d groups', groups.length)
+    for (var i = 0 ; i < groups.length; i += 3) {
+      var pkg = {}
+      add_pair(pkg, $(groups[i+0]))
+      add_pair(pkg, $(groups[i+1]))
+      add_pair(pkg, $(groups[i+2]))
+
+      if (!pkg.name)
+        return callback(new Error(`Cannot find name in groups: ${html}`))
+      if (!pkg.remaining)
+        return callback(new Error(`Cannot find remaining in groups: ${html}`))
+      if (!pkg.validity)
+        return callback(new Error(`Cannot find validity in groups: ${html}`))
+
+      var match = pkg.remaining.match(/(\d+\.?\d*) MB/)
+      if (match)
+        pkg.remaining = +match[1]
+      else if (pkg.remaining.match(/Unlimited/))
+        pkg.remaining = Infinity
+
+      result.push(pkg)
+    }
+
+    callback(null, result)
+  })
+}
+
+function add_pair(pkg, group) {
+  var label = group.find('label').text() || ''
+  var key = null
+  Object.keys(TH).forEach(function(field) {
+    if (label.match(TH[field]))
+      key = field
+  })
+
+  if (!key)
+    debug('No key for group: %s', group.html())
+  else {
+    pkg[key] = group.find('p.text-datain').text()
+  }
+}
+
+function fetch_balance(callback) {
   debug('Check balance')
   var url = `${SITE}/datausage/vas?device=web&version=`
   var headers = {'user-agent':UA, 'X-Requested-With':'XMLHttpRequest', referer:`${SITE}/`}
@@ -20,7 +76,7 @@ function check_balance(callback) {
     if (res.body.message != 'Success')
       return callback(new Error(`Unknown response: ${JSON.stringify(res.body)}`))
 
-    debug('Response HTML', res.body.html)
+    debug('Response HTML: %j', res.body.html)
     callback(null, res.body.html)
   })
 }
