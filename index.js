0ceff538421af4c1e069b8fd3d28b4c7cd6cb7b0
Jason Smith (iMac) <jason.h.smith@gmail.com>
2015-07-25 23:37:18 +0700

    Some headers: user-agent, referer, XHR

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var debug = require('debug')('hlife')
 var request = require('request')
 
 var SITE = 'http://hlife.truecorp.co.th'
+var UA   = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0'
 
 function check_balance(callback) {
   debug('Check balance')
   var url = `${SITE}/datausage/vas?device=web&version=`
-  request({url:url, json:true}, function(er, res) {
+  var headers = {'user-agent':UA, 'X-Requested-With':'XMLHttpRequest', referer:`${SITE}/`}
+
+  request({url:url, headers:headers, json:true}, function(er, res) {
     if (er)
       return callback(er)
     if (res.statusCode != 200)
